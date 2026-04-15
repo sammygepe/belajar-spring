6d9f468b70d0d096a5c7aa97a7bc90082310c3b3
Sammy Gunawan <sammy.gp@gmail.com>
2026-04-15 16:17:07 +0700

    add comment alur GetMapping

--- src/main/java/com/sammy/belajar_spring/HelloController.java
+++ src/main/java/com/sammy/belajar_spring/HelloController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+/*
+    1. lihat URL /hello
+    2. cari @GetMapping("/hello")
+    3. jalankan method hello()
+    4. kirim hasil ke browser
+*/
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello Spring Boot!";
