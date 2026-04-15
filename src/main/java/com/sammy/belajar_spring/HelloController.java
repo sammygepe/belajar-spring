0b183aa7673bfc1a1eabc331424d6aca39928a40
Sammy Gunawan <sammy.gp@gmail.com>
2026-04-15 15:35:12 +0700

    Initial commit

---
+++ src/main/java/com/sammy/belajar_spring/HelloController.java
@@ -0,0 +1,13 @@
+package com.sammy.belajar_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+}
